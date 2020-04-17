760beef8b4e78aadbce01d61bd2aceafea8dc27f
Alexandrew <andrew.alexandrow97@gmail.com>
2020-04-17 12:01:11 +0300

    initialize app

---
+++ src/sagas/app.js
@@ -0,0 +1,32 @@
+// outsource dependencies
+import {takeEvery, put, call} from 'redux-saga/effects';
+
+// local dependencies
+import TYPES from '../types';
+import * as axios from 'axios';
+
+function* setData() {
+  try {
+    yield put({type: TYPES.START_LOADING});
+
+    const response = yield call(getData);
+    yield put({type: TYPES.CALL_SUCCESS, payload: response.data});
+
+    yield put({type: TYPES.FINISH_LOADING});
+
+  } catch (e) {
+    yield put({type: TYPES.SHOW_ERROR});
+    yield put({type: TYPES.FINISH_LOADING});
+  }
+}
+
+async function getData() {
+  return axios({
+    method: 'get',
+    url: 'https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0',
+  });
+}
+
+export default function* () {
+  yield takeEvery(TYPES.SET_DATA, setData);
+}
